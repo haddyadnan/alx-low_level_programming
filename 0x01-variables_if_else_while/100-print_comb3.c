6a63c6cdb860c00258b7e21b84b7b403ecea1885
haddyadnan <haddyadnan@gmail.com>
2022-03-12 01:59:26 +0100

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Print combinations of two digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+  int first, second;
+
+  for (second = 0; second <= 9; second++)
+    {
+      for (first = second + 1; first <= 9; first++)
+	{
+	  putchar(second + '0');
+	  putchar(first + '0');
+
+	  if (second < 8)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
+	}
+    }
+  putchar('\n');
+
+  return (0);
+}
