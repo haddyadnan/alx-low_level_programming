688fa190c3dee5220cb943c07dfe2161ed5c88f4
haddyadnan <haddyadnan@gmail.com>
2022-03-15 16:26:28 +0100

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign
+ *
+ * @n: number
+ * Return: 1, 0, -1
+ *
+ */
+
+int print_sign(int n) :
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
