0963b309a65cdd44e69fe450d1f1fe4b74f0f36e
haddyadnan <haddyadnan@gmail.com>
2022-03-17 17:10:07 +0100

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * n: length of line
+ *
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}
