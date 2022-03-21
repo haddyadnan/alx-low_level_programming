989405766d5186e3a7bac6befe8a99d1964c9abb
haddyadnan <haddyadnan@gmail.com>
2022-03-21 16:29:10 +0100

    swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap integers
+ * @a: first integer
+ * @b: second integer
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
