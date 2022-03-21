0d94cb2419a0079d04432265ddb77c8ceaeec704
haddyadnan <haddyadnan@gmail.com>
2022-03-21 17:18:05 +0100

    put str

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 
 /**
  * _puts - prints string to stdout
  * @str: string
  */
 
-void _puts(char *str);
+void _puts(char *str)
 {
-	puts(s);
+	puts(str);
 }
