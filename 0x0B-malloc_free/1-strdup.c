ca538326c79ba99be6a184612b119f77903464d9
haddyadnan <haddyadnan@gmail.com>
2022-04-06 00:25:49 +0100

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strdup - returns pointer to a nwly allocated space
+ * @str: string
+ * Return: Pointer or NULL
+ *
+ */
+
+char *_strdup(char *str)
+{
+	return (strdup(str));
+}
