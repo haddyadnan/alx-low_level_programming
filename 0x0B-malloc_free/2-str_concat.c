f81342cee8ae1332a5407468b5d85f5f2ca7b40b
haddyadnan <haddyadnan@gmail.com>
2022-04-06 00:38:55 +0100

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,18 +15,29 @@ char *str_concat(char *s1, char *s2)
 	int i, j, c, d;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
+
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
 	for (i = 0; s1[i] != '\0'; i++)
+	{
 		;
+	}
 	for (j = 0; s2[j] != '\0'; j++)
+	{
 		;
+	}
 
 	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
 	if (a == NULL)
+	{
 		return (NULL);
+	}
 
 	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
