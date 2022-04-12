ea84032c31e4690f6b2afaccdcc7aeb92ed02db1
haddyadnan <haddyadnan@gmail.com>
2022-04-12 17:32:20 +0100

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_GUARDED_H
+#define DOG_GUARDED_H
+
+/**
+ * struct dog - Create a struct for dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
