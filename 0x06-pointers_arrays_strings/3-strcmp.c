00e981a133858796abcf14a2c6680319771ff6d5
haddyadnan <haddyadnan@gmail.com>
2022-04-19 15:36:36 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare str
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
