9216764a4f9c6f50bda93706ea2e7e1ef37dcfff
haddyadnan <haddyadnan@gmail.com>
2022-04-24 13:37:17 +0100

    caps string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+char *cap_string(char *n)
+{
+	int i;
+
+	if (n[0] >= 'a' && n[0] <= 'z')
+	{
+		n[0] = n[0] - 32;
+	}
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		switch (n[i])
+		{
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+		case '\n':
+		case '\t':
+		case ' ':
+			if (n[i + 1] > 96 && n[i + 1] < 123)
+			{
+				n[i + 1] = n[i + 1] - 32;
+			}
+		}
+	}
+	return (n);
+}
