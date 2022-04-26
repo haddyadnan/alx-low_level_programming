c7e76ddd70acd3019c633c29b61908f54b843834
haddyadnan <haddyadnan@gmail.com>
2022-04-26 18:08:04 +0100

    reverse linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -5,24 +5,6 @@
  * Return: what does it return?
  */
 
-/**
- * listint_len - find number of elements in the node
- *
- * @h: linked list
- * Return: i
- */
-
-size_t listint_len(const listint_t *h)
-{
-	size_t i;
-
-	for (i = 0; h; i++)
-	{
-		h = h->next;
-	}
-	return (i);
-}
-
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL, *next = NULL;
