a9efe5f701a2ef76e941355e4d63e901c0082099
haddyadnan <haddyadnan@gmail.com>
2022-04-29 15:18:55 +0100

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * @brief
  *
@@ -20,8 +20,8 @@ void print_binary(unsigned long int n)
 	for (; shifts >= 0; shifts--)
 	{
 		if ((n >> shifts) & 1)
-			_putchar("1");
+			_putchar('1');
 		else
-			_putchar("0");
+			_putchar('0');
 	}
 }
