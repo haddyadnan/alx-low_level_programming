db0aeffb6ac8181849436ad4648c8b8fa948a851
haddyadnan <haddyadnan@gmail.com>
2022-04-29 14:57:34 +0100

    binary to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*  */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum, exp;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (sum = 1, exp = 0, i--; i >= 0; i--, sum *= 2)
+	{
+		if (b[i] == '1')
+			exp += sum;
+	}
+
+	return (exp);
+}
