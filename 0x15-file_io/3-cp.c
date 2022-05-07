202bf6493915ae024011148fda002041a22fc885
haddyadnan <haddyadnan@gmail.com>
2022-05-07 03:28:43 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,8 +28,7 @@ int main(int ac, char *av[])
 	output_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (output_fd == -1)
 		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
-	do
-	{
+	do{
 		istatus = read(input_fd, buf, MAXSIZE);
 		if (istatus == -1)
 		{
