e8b2c363ee7f4c8d5245abe09f14c43e041426a7
haddyadnan <haddyadnan@gmail.com>
2022-05-16 18:19:18 +0100

    get ops

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -45,8 +45,12 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+
 	if (b == 0)
-		return (-1);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
@@ -59,7 +63,11 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+
 	if (b == 0)
-		return (-1);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
