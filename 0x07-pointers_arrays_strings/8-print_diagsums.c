47ce82a7775f577e0f9cd4262be16c6c93089861
haddyadnan <haddyadnan@gmail.com>
2022-05-23 13:25:54 +0100

    print diags

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums  - print sum of diags
+ *
+ * @a: matrix
+ * @size: size
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int rsmall = 0, rbig = 0;
+	int n = size * size;
+
+	for (i = 0; i < n; i += size + 1)
+	{
+		rsmall += a[i];
+	}
+
+	for (i = size - 1; i < s - 1; i += size - 1)
+	{
+		rbig += a[i];
+	}
+
+	printf("%d, %d\n", rsmall, rbig);
+}
