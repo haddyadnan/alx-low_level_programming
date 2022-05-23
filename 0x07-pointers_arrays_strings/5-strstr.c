48201a71901c8d0f49e287c97113d1e985c96a42
haddyadnan <haddyadnan@gmail.com>
2022-05-23 12:53:48 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: full string
+ * @needle: substring
+ * Return char*
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
