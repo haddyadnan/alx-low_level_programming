e51d99df0fd1bcd82f0dc3b77df1a6e48ac492dd
haddyadnan <haddyadnan@gmail.com>
2022-06-12 00:19:43 +0100

    sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	if (head == NULL)
+		return (0);
+
+	sum = 0;
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
