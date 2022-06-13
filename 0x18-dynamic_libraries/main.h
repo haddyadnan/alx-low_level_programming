39b6cbf35540eafdc162dcf808ec0e4634710a6e
haddyadnan <haddyadnan@gmail.com>
2022-06-13 15:18:15 +0100

    makewin

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -24,10 +24,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-/* int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int _div(int a, int b);
-int mod(int a, int b);
- */
+
 #endif /* DLL */
