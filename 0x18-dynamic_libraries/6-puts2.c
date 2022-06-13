09e50c34a9b375df08fe6d8fc7dd620855cad1f4
haddyadnan <haddyadnan@gmail.com>
2022-06-13 12:21:21 +0100

    DLL

---
+++ 0x18-dynamic_libraries/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - print every other character
+ * @str: string
+ *
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
