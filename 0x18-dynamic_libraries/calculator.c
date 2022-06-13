5cb1b15b8c188301af4d8c815414f0fbe02d5da6
haddyadnan <haddyadnan@gmail.com>
2022-06-13 15:08:26 +0100

    operations.so

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* #include "main.h"
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
