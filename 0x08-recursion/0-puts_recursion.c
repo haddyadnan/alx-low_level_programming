f93f7caaa0a2fa06bd7c56f8b1137628b9218ad7
haddyadnan <haddyadnan@gmail.com>
2022-06-17 12:17:23 +0100

    put recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * @brief
+ *
+ * @param s
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef RECURSION_H
+#define RECURSION_H
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif
