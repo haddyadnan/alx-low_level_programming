cddc97e8561135c927b26a8b041ed7191a9be68e
haddyadnan <haddyadnan@gmail.com>
2022-07-01 04:59:47 +0100

    set hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - should add element to hash table
+ *
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	/* To Be Completed */
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+	return (1)
+}
