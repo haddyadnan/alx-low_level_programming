dd98b7e1c857fe7a43335fb283372af4053109b8
haddyadnan <haddyadnan@gmail.com>
2022-07-02 18:24:01 +0100

    set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,5 +14,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/* To Be Completed */
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	return (1)
+	return (1);
 }
