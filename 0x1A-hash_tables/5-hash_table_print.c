684047948f74b5017c8321e55cb46d8b3fd901ad
haddyadnan <haddyadnan@gmail.com>
2022-07-02 05:13:56 +0100

    hash table print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print = print ht
+ *
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t dict;
+	unsigned long int i, flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		dict = ht->array[i];
+		if (dict != NULL)
+		{
+			if (flag != 0)
+				;
+			printf(", ");
+			flag = 1 printf("'%s': '%s'", dict->key, dict->value);
+			dict = dict->next;
+		}
+	}
+	printf("}\n");
+}
